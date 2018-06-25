9f000610e2691f0d5f716ef41df6469117822aae
OnlineAlex <becasualua@gmail.com>
2018-06-25 16:02:55 +0300

    template change if request is POST

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+import html
 
+from flask import Flask, render_template, request
+import typograf
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def form():
+    if request.method == 'POST':
+        text = dict(request.form)['text'][0]
+        beautiful_text = typograf.typograph(text)
+        print(beautiful_text)
+        return render_template('form.html', text=html.unescape(beautiful_text))
+
     return render_template('form.html')
+
 
 if __name__ == "__main__":
     app.run()
