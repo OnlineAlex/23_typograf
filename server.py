ab46898cc5caca87c5666a87daca1e4c71600152
OnlineAlex <becasualua@gmail.com>
2018-06-27 20:44:51 +0300

    add csrf, SECRET_KEY, WTF_CSRF_SECRET_KEY

--- server.py
+++ server.py
@@ -1,8 +1,17 @@
 import html
-
+import os
 from flask import Flask, render_template, request
+from flask_wtf.csrf import CSRFProtect
+
 import typograf
 app = Flask(__name__)
+csrf = CSRFProtect(app)
+
+csrf.init_app(app)
+app.config.update(dict(
+    SECRET_KEY=os.urandom(24),
+    WTF_CSRF_SECRET_KEY=os.urandom(24)
+))
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -10,7 +19,6 @@ def form():
     if request.method == 'POST':
         text = dict(request.form)['text'][0]
         beautiful_text = typograf.typograph(text)
-        print(beautiful_text)
         return render_template('form.html', text=html.unescape(beautiful_text))
 
     return render_template('form.html')
